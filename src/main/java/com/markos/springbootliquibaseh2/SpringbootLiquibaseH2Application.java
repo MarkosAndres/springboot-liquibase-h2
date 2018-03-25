e57dc8d59f072bb86045222e620f329f255de037
Markos Lerin <marcoslerin@gmail.com>
2018-03-25 12:53:18 -0600

    Initial Commit

---
+++ src/main/java/com/markos/springbootliquibaseh2/SpringbootLiquibaseH2Application.java
@@ -0,0 +1,12 @@
+package com.markos.springbootliquibaseh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLiquibaseH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLiquibaseH2Application.class, args);
+	}
+}
